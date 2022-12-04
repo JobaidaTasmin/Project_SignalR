edf1c70514a4b2fda56e853e6675e260126640ed
Jobaida Tasmin <106535311+JobaidaTasmin@users.noreply.github.com>
2022-12-04 20:56:04 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using MessengerApp.SMSHubs;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+app.MapHub<MessageAndFileShareHub>("/messageAndFileShareHub");
+app.Run();
